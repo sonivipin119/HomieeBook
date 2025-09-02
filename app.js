cdebed58ab3a5cae43f207d9716f0e9da2f9fc8a
sonivipin119 <sonivipin232@gmail.com>
2025-09-02 09:17:11 +0530

    correcting package.json

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const authcontroller = require("./controllers/auth");
 const storerouter = require("./routes/storerouter");
 const hostrouter = require("./routes/hostrouter");
 const authrouter = require("./routes/authrouter");
-const { error404} = require("./controllers/error");
+const { error404, error500} = require("./controllers/error");
 const User = require("./models/user");
 const root = require("./utils/pathutills");
 
